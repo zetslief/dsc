eb7085f87e38d1a2b303b7ec3713a2809e61cc53
Konstantin Konstantin <zetslief@protonmail.com>
2024-02-12 20:16:53 +0100

    Raylib!

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
-all:
-	gcc main.c -o main
+INCLUDE = -Iraylib/include
+LIBS = -Lraylib/libs
+
+main :
+	gcc main.c -o main $(INCLUDE) $(LIBS) -lraylib
+
+all : main
--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "stdio.h"
+#include "raylib/include/raylib.h"
 
 void main() {
-    printf("Hello, World\n");
+    InitWindow(800, 600, "Hello, raylib");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
 }
