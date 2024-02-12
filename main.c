5a170d289142b85d7c7412a4c4cb5dc0d16992d1
Konstantin Konstantin <zetslief@protonmail.com>
2024-02-12 20:28:01 +0100

    Center div.

--- main.c
+++ main.c
@@ -8,7 +8,14 @@ void main() {
     {
         BeginDrawing();
             ClearBackground(DARKGRAY);
-            DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+            int width = GetScreenWidth();
+            int height = GetScreenHeight();
+            int textHeight = 20;
+            const char* text = "Hello, Raylib!";
+            int textWidth = MeasureText(text, textHeight);
+            int x = width / 2 - textWidth / 2;
+            int y = height / 2 - textHeight / 2;
+            DrawText(text, x, y, textHeight, BLACK);
         EndDrawing();
     }
 
