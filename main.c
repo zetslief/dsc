62803c901787c10889b2c35c438805217aea9c5f
Konstantin Konstantin <zetslief@protonmail.com>
2024-02-15 23:47:19 +0100

    Drawing 4 pivots.

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "assert.h"
 #include "raylib/include/raylib.h"
 
 #define RADIUS 10
@@ -9,20 +11,35 @@ typedef struct Pivot {
 } Pivot;
 
 typedef struct Line {
-    Pivot first;
-    Pivot second;;
+    Pivot* pivots;
+    int pivotCount;
 } Line;
 
-Line create_line(float x1, float y1, float x2, float y2) {
-    Line line = {
-        .first = { .x = x1, .y = y1 },
-        .second = { .x = x2, .y = y2 },
-    };
+Pivot create_pivot(float x, float y ) {
+    Pivot pivot = { .x = x, .y = y };
+    return pivot;
+}
+
+Line create_line(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
+    Pivot* pivots = malloc(sizeof(Line) * 4);
+    pivots[0] = create_pivot(x1, y1);
+    pivots[1] = create_pivot(x2, y2);
+    pivots[2] = create_pivot(x3, y3);
+    pivots[3] = create_pivot(x4, y4);
+    Line line = { .pivots = pivots, .pivotCount = 4 };
     return line;
 }
 
+void delete_line(Line* line) {
+    assert(false && "delete_line is not implemented");
+}
+
 void main() {
-    Line line = create_line(100, 100, 700, 500);
+    Line line = create_line(
+            100, 100,
+            700, 100,
+            700, 500,
+            100, 500);
 
     InitWindow(800, 600, "Hello, raylib");
 
@@ -39,17 +56,14 @@ void main() {
             int y = height / 2 - textHeight / 2;
             DrawText(text, x, y, textHeight, BLACK);
 
-            DrawLine(
-                line.first.x,
-                line.first.y,
-                line.second.x,
-                line.second.y,
-                BLACK
-            );
-            DrawCircle(line.first.x, line.first.y, RADIUS, RED);
-            DrawCircle(line.second.x, line.second.y, RADIUS, RED);
+            for (int pivotIndex = 0; pivotIndex < line.pivotCount; ++pivotIndex) {
+                Pivot pivot = line.pivots[pivotIndex];
+                DrawCircle(pivot.x, pivot.y, RADIUS, RED);
+            }
         EndDrawing();
     }
 
     CloseWindow();
+
+    delete_line(&line);
 }
