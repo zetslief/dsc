26ed3a35fd7b1da40987a9ae5b98709a12449285
Konstantin Konstantin <zetslief@protonmail.com>
2024-02-17 12:19:51 +0100

    Building line.

--- main.c
+++ main.c
@@ -5,42 +5,46 @@
 
 #define RADIUS 10
 
-typedef struct Pivot {
+typedef struct {
     float x;
     float y;
 } Pivot;
 
-typedef struct Line {
+typedef struct {
     Pivot* pivots;
     int pivotCount;
 } Line;
 
-Pivot create_pivot(float x, float y ) {
-    Pivot pivot = { .x = x, .y = y };
-    return pivot;
+float calculate_line(float x, float a, float b) {
+    return a * x + b;
 }
 
-Line create_line(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
-    Pivot* pivots = malloc(sizeof(Line) * 4);
-    pivots[0] = create_pivot(x1, y1);
-    pivots[1] = create_pivot(x2, y2);
-    pivots[2] = create_pivot(x3, y3);
-    pivots[3] = create_pivot(x4, y4);
-    Line line = { .pivots = pivots, .pivotCount = 4 };
+Line create_line(Pivot start, Pivot stop, int steps) {
+    int total_length = 2 + steps;
+    float step = (stop.x - start.x) / steps;
+    float a = (stop.y - start.y) / (stop.x - start.x);
+    float b = start.y - a * start.x;
+    Pivot* pivots = malloc(sizeof(Line) * total_length);
+    pivots[0] = start;
+    for (int pivotIndex = 1; pivotIndex < total_length - 1; ++pivotIndex) {
+        float x = start.x + step * pivotIndex;
+        float y = calculate_line(x, a, b);
+        Pivot pivot = { .x = x, .y = y };
+        pivots[pivotIndex] = pivot;
+    }
+    pivots[total_length - 1] = stop;
+    Line line = { .pivots = pivots, .pivotCount = total_length };
     return line;
 }
 
 void delete_line(Line* line) {
-    assert(false && "delete_line is not implemented");
+    free(line->pivots);
 }
 
 void main() {
-    Line line = create_line(
-        100, 100,
-        700, 100,
-        700, 500,
-        100, 500
-    );
+    Pivot start = { .x = 100, .y = 100 };
+    Pivot stop = { .x = 700, .y = 500 };
+    Line line = create_line(start, stop, 10);
 
     InitWindow(800, 600, "Hello, raylib");
 
@@ -48,18 +52,10 @@ void main() {
     {
         BeginDrawing();
             ClearBackground(DARKGRAY);
-            int width = GetScreenWidth();
-            int height = GetScreenHeight();
-            int textHeight = 20;
-            const char* text = "Hello, Raylib!";
-            int textWidth = MeasureText(text, textHeight);
-            int x = width / 2 - textWidth / 2;
-            int y = height / 2 - textHeight / 2;
-            DrawText(text, x, y, textHeight, BLACK);
 
-            for (int pivotIndex = 0; pivotIndex < line.pivotCount; ++pivotIndex) {
+            for (int pivotIndex = 0; pivotIndex < line.pivotCount - 1; ++pivotIndex) {
                 Pivot currentPivot = line.pivots[pivotIndex];
-                Pivot nextPivot = line.pivots[(pivotIndex  + 1) % line.pivotCount];
+                Pivot nextPivot = line.pivots[pivotIndex  + 1];
                 DrawLine(
                     currentPivot.x, currentPivot.y,
                     nextPivot.x, nextPivot.y,
@@ -71,6 +67,15 @@ void main() {
                 Pivot pivot = line.pivots[pivotIndex];
                 DrawCircle(pivot.x, pivot.y, RADIUS, RED);
             }
+
+            int width = GetScreenWidth();
+            int height = GetScreenHeight();
+            int textHeight = 20;
+            const char* text = "Hello, Raylib!";
+            int textWidth = MeasureText(text, textHeight);
+            int x = width / 2 - textWidth / 2;
+            int y = height / 2 - textHeight / 2;
+            DrawText(text, x, y, textHeight, BLACK);
         EndDrawing();
     }
 
